0d9143837423c36d49be75c0a0adac3a0dc923ef
cchu42 <cchu42@myseneca.ca>
2023-01-20 22:18:21 -0500

    setup passport

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 
+// For passport use
+const passport = require('passport');
+const authenticate = require('./authentication');
+
 const logger = require('./logger');
 const pino = require('pino-http')({
   // Use our default logger instance, which is already configured
@@ -23,6 +27,10 @@ app.use(cors());
 
 // Use gzip/deflate compression middleware
 app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
 // Define our routes
 app.use('/', require('./routes'));
