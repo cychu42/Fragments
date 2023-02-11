de2f7a31e05caad5aa203e444a289d1060c4aa42
cchu42 <cchu42@myseneca.ca>
2023-02-11 16:48:51 -0500

    Add POST /v1/fragments route

---
+++ src/routes/api/post.js
@@ -0,0 +1,44 @@
+const { createSuccessResponse } = require('../../response.js');
+const { createErrorResponse } = require('../../response.js');
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
+/**
+ * Get a list of fragments for the current user
+ */
+module.exports = async (req, res) => {
+  let content;
+  if (Buffer.isBuffer(req.body)) {
+    const newFrag = new Fragment({
+      ownerId: req.get('authorization'),
+      type: req.get('Content-Type'),
+      size: 0,
+    });
+    try {
+      await newFrag.save();
+      await newFrag.setData(req.body);
+      content = await Fragment.byId(req.get('authorization'), newFrag.id);
+    } catch (e) {
+      logger.warn('Error with byId():', e);
+    }
+    // Setting Location header
+    const apiUrl = new URL(
+      `/v1/fragments/${newFrag.id}`,
+      `https://${process.env.API_URL || req.headers.host}`
+    );
+    res.setHeader('Location', apiUrl);
+
+    logger.debug(`id of the fragment is ${newFrag.id}`);
+    logger.debug(`req.headers.host is ${req.headers.host}`);
+    logger.debug(`apiUrl is ${apiUrl}`);
+
+    const response = createSuccessResponse({
+      fragment: content,
+    });
+    logger.info(`Content-Type is supported; sending a response from POST /v1/fragments`);
+    res.status(201).json(response);
+  } else {
+    logger.info(`Content-Type is not supported.`);
+    const response = createErrorResponse(415, 'Content-Type is not supported.');
+    res.status(415).json(response);
+  }
+};
---
+++ tests/unit/post.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('POST /v1/fragments', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  // Using a valid username/password pair should give a success result
+  test('authenticated users can get a text/plain fragment', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .send('This is a fragment')
+      .set('Content-Type', 'text/plain');
+    expect(res.statusCode).toBe(201);
+    expect(res.body.status).toBe('ok');
+    expect(res.body.fragment).toEqual(expect.any(Object));
+  });
+
+  test('authenticated users get a fragment back with correct type, created, updated, size, id, ownerId, and Location header', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .send('This is a fragment')
+      .set('Content-Type', 'text/plain');
+    expect(res.body.fragment.type).toEqual('text/plain');
+    expect(res.body.fragment.created).toEqual(res.body.fragment.updated);
+    expect(res.get('Location').endsWith(`/v1/fragments/${res.body.fragment.id}`)).toBe(true);
+    expect(res.body.fragment.size).toEqual(expect.any(Number));
+    expect(res.body.fragment.id).toEqual(expect.any(String));
+    expect(res.body.fragment.ownerId).toEqual(expect.any(String));
+  });
+  test('trying to create a fragment with an unsupported type returns a 415 error message', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .send('This is a fragment')
+      .set('Content-Type', 'bad/type');
+    expect(res.statusCode).toBe(415);
+    expect(res.body.status).toBe('error');
+    expect(res.body.error).toEqual(expect.any(Object));
+  });
+});
